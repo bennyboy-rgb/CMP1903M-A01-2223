e71a441f33603fab517e57ced5f232308cf089f6
bennyboy-rgb <124790553+bennyboy-rgb@users.noreply.github.com>
2023-04-25 12:10:07 +0100

    new method

--- CMP1903M A01 2223/operation.cs
+++ CMP1903M A01 2223/operation.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CMP1903M_A01_2223
 {
-    internal class operation
+     class operation
     {
+          public double _num1;
+         public double _num2;
+         public int _operand;
+        
+        public operation(double num1, double num2, int operand)
+        {
+           _num1 = num1;
+            _num2 = num2;
+            _operand = operand;
+
+        }
+
+        public double performance()
+        {
+          if (_operand == 1)
+            {
+                return _num1 + _num2;
+            }
+          else if (_operand == 2)
+            {
+                return _num1 - _num2;
+            }
+          else if (_operand == 3)
+            {
+                return _num1 * _num2;
+            }
+          else if(_operand == 4)
+            {
+                return _num1 * _num2;
+            }
+            return 0;
+        }
     }
 }
--- CMP1903M A01 2223/testing.cs
+++ CMP1903M A01 2223/testing.cs
@@ -11,8 +11,19 @@ namespace CMP1903M_A01_2223
         static void Main(string[] args)
         {
             Pack pack = new Pack();
-            MainMenu.menu();
-            
+            //MainMenu.menu();
+
+            List<Card> list = Pack.dealCard(3);
+            foreach (Card card in list)
+            {
+                Console.WriteLine(card);
+            }
+           operation operate = new operation(list[0].Value, list[2].Value, list[1].Suit);
+            double x = operate.performance();
+            Console.WriteLine(x);
+
+
+
             /*Help.firstmessage();
             Help.operators();
             Help.sample();
